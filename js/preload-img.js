3ab4f746f30a584833b8e4887a5a33381944f4e0
farmconom <farmconom@gmail.com>
2023-03-18 02:06:55 +0700

    fix preload

--- js/preload-img.js
+++ js/preload-img.js
@@ -1,10 +1,10 @@
-const images = ['../images/bw-icon.jpg',
-                '../images/service-analytics.jpg',
-                '../images/service-cloud.jpg',
-                '../images/service-love.jpg',
-                '../images/team-antonio.jpg',
-                '../images/team-justin.jpg',
-                '../images/team-mark.jpg'];
+const images = ['/Black-and-White-Website-Demo/images/bw-icon.jpg',
+                '/Black-and-White-Website-Demo/images/service-analytics.jpg',
+                '/Black-and-White-Website-Demo/images/service-cloud.jpg',
+                '/Black-and-White-Website-Demo/images/service-love.jpg',
+                '/Black-and-White-Website-Demo/images/team-antonio.jpg',
+                '/Black-and-White-Website-Demo/images/team-justin.jpg',
+                '/Black-and-White-Website-Demo/images/team-mark.jpg'];
 
 images.forEach((image) => {
   const img = new Image();
